3c971d1fe6a05dc62dbf86ddde605b865a82438b
scripto1 <heeyongi@gmail.com>
2024-03-22 15:05:05 +0900

    타입 색상 수정

--- src/main.js
+++ src/main.js
@@ -3,70 +3,90 @@ const pokemons = [
 		id: 1,
 		name: '이상해씨',
 		type1: '풀',
+		classType1: 'grass',
 		type2: '독',
+		classType2: 'poison',
 		image: 'img/000101.png',
 	},
 	{
 		id: 2,
 		name: '파이리',
 		type1: '불꽃',
+		classType1: 'fire',
 		type2: '',
+		classType2: '',
 		image: 'img/000401.png',
 	},
 	{
 		id: 3,
 		name: '꼬부기',
 		type1: '물',
+		classType1: 'water',
 		type2: '',
+		classType2: '',
 		image: 'img/000701.png',
 	},
 	{
 		id: 4,
 		name: '캐터피',
 		type1: '벌레',
+		classType1: 'insect',
 		type2: '',
+		classType2: '',
 		image: 'img/001001.png',
 	},
 	{
 		id: 5,
 		name: '단데기',
 		type1: '벌레',
+		classType1: 'insect',
 		type2: '',
+		classType2: '',
 		image: 'img/001101.png',
 	},
 	{
 		id: 6,
 		name: '뿔충이',
 		type1: '벌레',
+		classType1: 'insect',
 		type2: '독',
+		classType2: 'poison',
 		image: 'img/001301.png',
 	},
 	{
 		id: 7,
 		name: '독침붕',
 		type1: '벌레',
+		classType1: 'insect',
 		type2: '독',
+		classType2: 'poison',
 		image: 'img/001501.png',
 	},
 	{
 		id: 8,
 		name: '구구',
 		type1: '노말',
+		classType1: 'normal',
 		type2: '비행',
+		classType2: 'fly',
 		image: 'img/001601.png',
 	},
 	{
 		id: 9,
 		name: '레트라',
 		type1: '노말',
+		classType1: 'normal',
 		type2: '',
+		classType2: '',
 		image: 'img/002002.png',
 	},
 	{
 		id: 10,
 		name: '피카츄',
 		type1: '전기',
+		classType1: 'electric',
 		type2: '',
+		classType2: '',
 		image: 'img/002501.png',
 	},
 ]
@@ -85,8 +105,8 @@ function showList(val='') {
 				</div>				
 				<p>${pokemon.name}</p>
 				<div class="type-cont">
-					<div class="type1">${pokemon.type1}</div>					
-					<div class="type2">${pokemon.type2}</div>					
+					<div class="type1-${pokemon.classType1}">${pokemon.type1}</div>					
+					<div class="type2-${pokemon.classType2}">${pokemon.type2}</div>					
 				</div>			
 			`
 			list.appendChild(li)
@@ -98,8 +118,8 @@ function showList(val='') {
 				</div>				
 				<p>${pokemon.name}</p>
 				<div class="type-cont">
-					<div class="type1">${pokemon.type1}</div>					
-					<div class="type2">${pokemon.type2}</div>					
+					<div class="type1-${pokemon.classType1}">${pokemon.type1}</div>					
+					<div class="type2-${pokemon.classType2}">${pokemon.type2}</div>					
 				</div>			
 			`
 			list.appendChild(li)
@@ -111,8 +131,8 @@ function showList(val='') {
 				</div>				
 				<p>${pokemon.name}</p>
 				<div class="type-cont">
-					<div class="type1">${pokemon.type1}</div>					
-					<div class="type2">${pokemon.type2}</div>					
+					<div class="type1-${pokemon.classType1}">${pokemon.type1}</div>					
+					<div class="type2-${pokemon.classType2}">${pokemon.type2}</div>					
 				</div>			
 			`
 			list.appendChild(li)
@@ -122,12 +142,12 @@ function showList(val='') {
 showList()
 
 // 타입에 따라 색상 변경
-const type1El = document.querySelector('.type1');
+// const type1El = document.querySelector('.type1');
 
-pokemons.forEach((pokemon) => {
-  const type1Class = `type1-${pokemon.type1}`;
-  type1El.classList.add(type1Class);
-});
+// pokemons.forEach((pokemon) => {
+//   const type1Class = `type1-${pokemon.type1}`;
+//   type1El.classList.add(type1Class);
+// });
 
 // 검색 기능
 const searchInput = document.getElementById('search');
